2bd03785ba2e501fe1425e515ed2f59e523114bb
Samuel Curry <samc735@gmail.com>
2022-11-11 23:50:59 -0500

    rules

--- src/lib/routes/config-store/index.js
+++ src/lib/routes/config-store/index.js
@@ -2,16 +2,16 @@ const logger = require('../../logger');
 const {DiscordGuildRule} = require('../../database/models');
 
 module.exports = async (fastify, opts) => {
-    fastify.patch('/discord-guild/:id', {}, async (req, reply) => {
-        logger.info(`Received update request for rule ${req.body.rule.name} in guild [discordGuildId=${req.params.id}] to be ${req.body.rule.enabled}`);
-        let rule = await DiscordGuildRule.findOne({where: {ruleName: req.body.rule.ruleName, discordGuildId: req.params.id}});
+    fastify.patch('/discord-guild/:id/rule/:ruleId', {}, async (req, reply) => {
+        logger.info(`Received update request for rule [ruleId=${req.params.ruleId}] in guild [discordGuildId=${req.params.id}] to be ${req.body.rule.enabled}`);
+        let rule = await DiscordGuildRule.findOne({where: {ruleId: req.params.ruleId, discordGuildId: req.params.id}});
 
         if(!rule){
             const createdRule = await new DiscordGuildRule({...req.body.rule, discordGuildId: req.params.id}).save();
             return {DiscordGuildRule: createdRule.get()};
         }
 
-        rule = await DiscordGuildRule.update({enabled: req.body.rule.enabled, ruleAction: req.body.rule.ruleAction}, {where: {discordGuildId: req.params.id}, returning: true});
+        rule = await DiscordGuildRule.update({enabled: req.body.rule.enabled, ruleAction: req.body.rule.ruleAction}, {where: {discordGuildId: req.params.id, ruleId: req.params.ruleId}, returning: true});
 
         return {DiscordGuildRule: rule.get()};
     });
